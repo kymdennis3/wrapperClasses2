967a5005eb1b0fd06e4e71db0bfd87927aca0075
Kimtai <kymdennis3@gmail.com>
2023-01-13 06:32:29 +0300

    unboxing - Even though this is a wrapper class, it still works as a primitive data type

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /* wrapper class = provides a way to use primitive data types as reference data types
+                           reference data types contain useful methods
+                           can be used with collections (ex.ArrayList)
+        */
+
+        /* primitive    |      wrapper
+        -------------------------------
+        boolean              Boolean
+        int                  Integer
+        double               Double
+        char                 Character
+
+        autoboxing = the automatic conversion that the Java Compiler makes between the primitive  data types and their corresponding object wrapper classes
+        unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive.
+         */
+
+        Boolean a = true;
+        Integer b = 123;
+        Character c = '@';
+        Double d = 3.14;
+        String e = "Bro";
+
+        if(a==true){
+            System.out.println("This is true");
+        }
+
     }
 }
