83481bc94241decd1098c41accf3acc6ae9e7257
Kimtai <kymdennis3@gmail.com>
2023-01-13 06:34:22 +0300

    unboxing - Even though this is a wrapper class, it still works as a primitive data type

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
         double               Double
         char                 Character
 
+        Wrapper classes contain useful methods but also takes time to be accessed hence more time and processing power needed
+
         autoboxing = the automatic conversion that the Java Compiler makes between the primitive  data types and their corresponding object wrapper classes
         unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive.
          */
